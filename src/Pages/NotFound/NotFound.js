e43ffb9252f86fd2d8e77105d2137439e4baf731
raiyan <raiyan5764@gmail.com>
2021-11-12 11:55:54 +0600

    some changes done

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,51 @@
+import { motion } from "framer-motion";
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 page not found</h1>
-    </div>
+    <section className="page-not">
+      <Container>
+        <Row className="py-4">
+          <Col xs={12} lg={12} md={12}>
+            <div className="back-btn">
+              <motion.button
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 1 }}
+                onClick={() => window.history.back()}
+              >
+                {" "}
+                <BsArrowLeft size={20} className="me-2" /> Back
+              </motion.button>
+            </div>
+          </Col>
+          <Col xs={12} lg={12} md={12}>
+            <div className="not-found-image d-flex justify-content-center align-items-center flex-column">
+              <div>
+                <img
+                  src="https://i.ibb.co/rmGBnd2/404-Error-bro.png"
+                  alt=""
+                  width="500px"
+                />
+              </div>
+              <Link className="text-decoration-none" to="/home">
+                <motion.button
+                  className="d-block btn btn-outline-dark m-auto py-2 px-5"
+                  onClick={() => window.scrollTo(0, 0)}
+                  whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 1 }}
+                >
+                  Home
+                </motion.button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
